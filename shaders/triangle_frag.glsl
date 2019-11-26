bb3342241358408b0dc05a886c8874dd0fe9ced2
Henry Loh <pgloh.henry@gmail.com>
2019-11-26 13:41:26 -0800

    Added vec3 rgb to shader

--- shaders/triangle_frag.glsl
+++ shaders/triangle_frag.glsl
@@ -3,6 +3,9 @@
 in vec3 out_pos;
 out vec4 FragColor;
 
+uniform vec3 rgb;
+
 void main () {
-  FragColor = vec4(out_pos.z, out_pos.z, out_pos.z, 1);
+  FragColor = vec4(rgb, 1.0);
+//  FragColor = vec4(out_pos.z, out_pos.z, out_pos.z, 1);
 }
